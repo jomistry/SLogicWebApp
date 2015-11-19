1d18c57d6c345a731e77f761c9d44ed98251a326
Jo Mistry <jo.mistry@man.com>
2015-11-19 18:04:57 +0000

    add paging

--- app/modules/app.js
+++ app/modules/app.js
@@ -5,7 +5,12 @@ angular.module('mainApp', ['ngRoute', 'controllersModule'])
             controller: 'searchResultsCtrl',
             controllerAs: 'searchResults'
         })
-        r.when('/:searchterm', {
+        .when('/:searchterm/:pagenum', {
+            templateUrl: 'views/searchResults.html',
+            controller: 'searchResultsCtrl',
+            controllerAs: 'searchResults'
+        })      
+        .when('/:searchterm', {
             templateUrl: 'views/searchResults.html',
             controller: 'searchResultsCtrl',
             controllerAs: 'searchResults'
--- app/modules/controllers.js
+++ app/modules/controllers.js
@@ -3,22 +3,21 @@ angular.module('controllersModule', [])
 
         var self = this;
         self.pageInfo = {
-                per_page: 12,
-                current_page: 0,
-                prev_page: null,
-                total_pages: -1,
-                total_count: -1,
-                next_page: 1
-                };
-                
+            per_page: 12,
+            current_page: $routeParams.pagenum == null ? 1 : $routeParams.pagenum,
+            prev_page: null,
+            query: $routeParams.searchterm == null ? 'A' : $routeParams.searchterm, 
+            next_page: 1
+        };
+
         self.perpage = 12;
 
         $http.get(
-                'https://www.quandl.com/api/v3/datasets.json?database_code=WIKI&query=' + $routeParams.searchterm + '&per_page=' +  self.pageInfo.per_page + '&page=' + self.pageInfo.next_page
-        ).success(function (data) {
+            'https://www.quandl.com/api/v3/datasets.json?database_code=WIKI&query=' + self.pageInfo.query + '&per_page=' + self.pageInfo.per_page + '&page=' + self.pageInfo.current_page
+            ).success(function (data) {
                 self.datasets = data.datasets;
-                self.pageInfo = data.meta; 
+                self.pageInfo = data.meta;
             })
-            .error();
+            .error(function (e) {console.log(e); });
 
     }]);
